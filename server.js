a4d7b875c4f3a7500638cf3d0e8c5a8f8d81e156
jackdelv <jackdelv39@gmail.com>
2021-07-01 12:01:10 -0400

    Added post route handler for creating a shortened url.

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+var mongoose = require('mongoose');
+var bodyParser = require("body-parser");
+var dns = require('dns');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+const urlSchema = new mongoose.Schema({
+  shortUrl: Number,
+  longUrl: String
+});
 
+var URL = mongoose.model('URL', urlSchema);
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -17,6 +27,26 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', function(req, res, done) {
+  var longUrl = req.body.url;
+  dns.lookup(longUrl, function(err, data) {
+    if (err) {
+      res.json({error: err});
+      return console.log(err);
+    }
+    URL.find({}, function(err, URLS){
+       if (err) return console.log(err);
+       var shortUrl = URLS.length + 1;
+       var url = new URL({shortURL: shortUrl, longUrl: longUrl});
+       url.save(function(err, data) {
+         if (err) return console.log(err);
+         res.json({original_url: longUrl, short_url: shortUrl});
+         done(err, data);
+      });
+    }); 
+  });
 });
 
 app.listen(port, function() {
