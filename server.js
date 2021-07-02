07307f6c89cd965a3ea4a04bf1601cf8b1bdbea4
jackdelv <jackdelv39@gmail.com>
2021-07-02 13:49:23 -0400

    Finished api

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const express = require('express');
 const cors = require('cors');
 var mongoose = require('mongoose');
 var bodyParser = require("body-parser");
-var dns = require('dns');
+const dns = require('dns');
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 const urlSchema = new mongoose.Schema({
-  shortUrl: Number,
-  longUrl: String
+  original_url: String,
+  short_url: Number  
 });
 
 var URL = mongoose.model('URL', urlSchema);
@@ -30,22 +32,47 @@ app.get('/api/hello', function(req, res) {
 });
 
 app.post('/api/shorturl', function(req, res, done) {
-  var longUrl = req.body.url;
-  dns.lookup(longUrl, function(err, data) {
+  var original_url = req.body.url;
+  if (original_url.search(/http(s|):\/\//i) == -1) {
+    res.json({error: 'invalid url'});
+  }
+  console.log(original_url.replace(/http(s|):\/\//i, '').replace(/\/\S*/, ''));
+  dns.lookup(original_url.replace(/http(s|):\/\//i, '').replace(/\/\S*/, '') , function(err, data) {
     if (err) {
-      res.json({error: err});
+      res.json({error: 'invalid url'});
       return console.log(err);
     }
-    URL.find({}, function(err, URLS){
-       if (err) return console.log(err);
-       var shortUrl = URLS.length + 1;
-       var url = new URL({shortURL: shortUrl, longUrl: longUrl});
-       url.save(function(err, data) {
-         if (err) return console.log(err);
-         res.json({original_url: longUrl, short_url: shortUrl});
-         done(err, data);
-      });
-    }); 
+    URL.findOne({original_url: original_url}, function(err, data){
+      if (err) return console.log(err);
+      if (data) {
+        res.json({original_url: data.original_url, short_url: data.short_url});
+        done(null, data);
+      } else {
+        URL.find({}, function(err, URLS){
+           if (err) return console.log(err);
+           var short_url = URLS.length + 1;
+           var url = new URL({short_url: short_url, original_url: original_url});
+           url.save(function(err, data) {
+             if (err) return console.log(err);
+             res.json({original_url: original_url, short_url: short_url});
+             done(null, data);
+           });
+        });
+      }
+    });
+     
+  });
+});
+
+app.get('/api/shorturl/:short_url', function(req, res, done) {
+  URL.findOne({short_url: req.params.short_url}, function(err, data) {
+    if(err) return console.log(err);
+    if(data){
+      res.redirect(data.original_url);
+    } else {
+      res.json({error: "No short URL found for the given input"});
+    }
+    done(err, data);
   });
 });
 
